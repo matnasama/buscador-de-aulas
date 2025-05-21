81ea9f1d31e143dac41d976b6e0115cf2b34e218
matnasama <110042114+matnasama@users.noreply.github.com>
2025-05-21 17:48:03 -0300

    para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    port: 4173,
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  base: '/',
 })
